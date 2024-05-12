8b69097bc643292a9af5093d09ea54909fb3c522
DelfinaSvetliza <delfina.svetliza@gmail.com>
2024-05-12 16:28:36 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prySvetlizaEtapa3
+{
+    public partial class Form1 : Form
+    {
+        private PictureBox imgAuto;
+        private PictureBox imgAvion;
+        private PictureBox imgBarco;
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            // Crear los PictureBox y configurar sus propiedades
+            imgAuto = new PictureBox();
+            imgAuto.Image = Properties.Resources.auto;
+            imgAuto.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgAuto.Visible = false;
+
+            imgAvion = new PictureBox();
+            imgAvion.Image = Properties.Resources.avion;
+            imgAvion.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgAvion.Visible = false;
+
+            imgBarco = new PictureBox();
+            imgBarco.Image = Properties.Resources.barco;
+            imgBarco.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgBarco.Visible = false;
+        }
+        public void CargarImagenes()
+        {
+            // Asignar la imagen del recurso a cada PictureBox
+            
+            
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.C: // Tecla "C" para el auto
+                    MostrarImagen(imgAuto);
+                    break;
+                case Keys.B: // Tecla "B" para el barco
+                    MostrarImagen(imgBarco);
+                    break;
+                case Keys.P: // Tecla "P" para el avión
+                    MostrarImagen(imgAvion);
+                    break;
+                default:
+                    // Si se presiona una tecla diferente, no se hace nada
+                    break;
+
+            }
+        }
+
+        // Método para mostrar la imagen en el formulario
+        private void MostrarImagen(PictureBox pictureBox)
+        {
+            // Agregar el PictureBox al formulario si aún no está agregado
+            if (!this.Controls.Contains(pictureBox))
+            {
+                this.Controls.Add(pictureBox);
+                pictureBox.Dock = DockStyle.Fill;
+            }
+
+            // Mostrar el PictureBox y traerlo al frente
+            pictureBox.Visible = true;
+            pictureBox.BringToFront();
+        }
+    }
+}
+
+    
+    
+
---
+++ clsVehiculo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace prySvetlizaEtapa3
+{
+    internal class clsVehiculo
+    {
+        public string nombre { get; set; }
+        public string tipo { get; set; }
+        public PictureBox imgAuto { get; set; }
+        public PictureBox imgAvion { get; set; }
+        public PictureBox imgBarco { get; set; }
+
+
+        public clsVehiculo(string nombre, string tipo, Image imagen)
+        {
+            this.nombre = nombre;
+            this.tipo = tipo;
+            imgAuto = imgAuto;
+            imgAvion = imgAvion;
+            imgBarco = imgBarco;
+        }
+        public void crearVehiculo()
+        {
+            imgAuto = new PictureBox();
+            imgAvion = new PictureBox();
+            imgBarco = new PictureBox();
+            imgAuto.Image=  Properties.Resources.auto;
+            imgAvion.Image = Properties.Resources.avion;
+            imgBarco.Image = Properties.Resources.barco;
+            imgAuto.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgAvion.SizeMode = PictureBoxSizeMode.StretchImage;
+            imgBarco.SizeMode = PictureBoxSizeMode.StretchImage;
+            MessageBox.Show($"Vehículo creado: {nombre}, Tipo: {tipo}");
+        }
+    }
+}
